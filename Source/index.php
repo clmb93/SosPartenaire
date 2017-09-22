f23acb1e7857ada053f349fd20239dcbf437198a
julien <julien.ladisa@gmail.com>
2017-09-22 10:39:56 +0200

    Ajout des flux rss

--- Source/index.php
+++ Source/index.php
@@ -3,6 +3,7 @@
         <meta charset="utf-8">
         <title>SOS Partenaires</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="lib/js/FeedEk/css/FeedEk.css">
         <link rel="stylesheet" href="public/css/index.css">
         
         
@@ -20,7 +21,7 @@
             <div class="col-lg-12">
                 <div class="col-lg-2 contenu"></div>
                 <div class="col-lg-8 contenu "></div>
-                <div class="col-lg-2 contenu"></div>
+                <div class="col-lg-2 contenu"  id="DivRss"></div>
             </div>
         </div>
         <div class="row">
@@ -32,6 +33,7 @@
     </body> 
     <footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="lib/js/FeedEk/js/FeedEk.js"></script>
         <script src="public/js/index.js"></script>
     
     </footer>
---
+++ Source/public/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   
+    $('#DivRss').FeedEk({
+        FeedUrl:'http://rmcsport.bfmtv.com/rss/info/flux-rss/flux-toutes-les-actualites/',
+        MaxCount : 5,
+        ShowDesc : true,
+        ShowPubDate:true,
+        DescCharacterLimit:100,
+        TitleLinkTarget:'_blank',
+        DateFormat: 'MM/DD/YYYY',
+
+    });
+    
+});
