8f074d11b1ca67b45c7b36bd9517fc1b1ad2b075
LADISA Julien <j.ladisa@Accedia.local>
2017-09-27 12:18:44 +0200

    correction bug index.js flux rss

--- Source/public/js/index.js
+++ Source/public/js/index.js
@@ -1,16 +1,13 @@
-$(document).ready(function(){
-   
-    
-    
-    
-    $('#DivRss').FeedEk({                                               //création du flux rss
-        FeedUrl:'http://www.sports.fr/fr/cmc/rss.xml',
-        MaxCount : 30,
-        ShowDesc : true,
-        ShowPubDate:true,
-        DescCharacterLimit:100,
-        TitleLinkTarget:'_blank',
+ $(document).ready(function(){
+        $('#DivRss').FeedEk({                                               //création du flux rss
+            FeedUrl:'http://www.sport.fr/feed',
+            MaxCount : 30,
+            ShowDesc : true,
+            ShowPubDate:true,
+            DescCharacterLimit:100,
+            TitleLinkTarget:'_blank',
+        
 
-    });
-    
+        });
+
 });
