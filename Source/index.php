2bc89ef6b8b44c2237fe9d86e73e4b4f4b070889
LADISA Julien <j.ladisa@Accedia.local>
2017-09-27 15:57:54 +0200

    changements mineurs

--- Source/index.php
+++ Source/index.php
@@ -5,25 +5,23 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="lib/js/FeedEk/css/FeedEk.css">
         <link rel="stylesheet" href="public/css/index.css">
-        <link rel="icon" href="public/img/logo/logo-icon.png">
-        
-        
+        <link rel="icon" href="public/img/logo/logo-icon.png">    
     </head> 
     <body class="container-fluid">
-            <header>
-                <div class="col-lg-2 header">
-                   <img src="public/img/logo/logo.png" alt="logo" class="bg-header">
-                </div>
-                <div class="col-lg-10 header">
+        <header>
+        	<div class="col-lg-2 header">
+            	<img src="public/img/logo/logo.png" alt="logo" class="bg-header">
+            </div>
+            <div class="col-lg-10 header">
                     nav
-                </div>
-            </header> 
+            </div>
+        </header> 
         <section class="row">
             <div class="col-lg-12">
-                <div class="col-lg-2 contenu"></div>
+				<div class="col-lg-2 contenu"></div>
                 <div class="col-lg-8 contenu "></div>
                 <div class="col-lg-2 contenu flux_rss">
-                    <h3 id="Actu_rss">Actualités sportives</h3>
+					<h3 id="Actu_rss">Actualités sportives</h3>
                     <div id="DivRss"></div>
                     <a href="http://www.sports.fr/" target='_blank' id="rss_title">Afficher plus</a>
                 </div>
@@ -34,12 +32,10 @@
                 <div class="col-lg-12 footer"></div>
             </div>
         </div>
-        
     </body> 
     <footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="lib/js/FeedEk/js/FeedEk.js"></script>
         <script src="public/js/index.js"></script>
-    
     </footer>
 </html>
