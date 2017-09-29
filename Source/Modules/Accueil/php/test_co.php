2810bd9e4a711727b14d7bf554294ab2d21a9c2a
LADISA Julien <j.ladisa@Accedia.local>
2017-09-29 15:17:58 +0200

    création page 403 et ajout d'un script de protection de la page test_co.php

--- Source/Modules/Accueil/php/test_co.php
+++ Source/Modules/Accueil/php/test_co.php
@@ -1,21 +1,39 @@
 <?php
+/* protection script */
+
+if(!isset($_POST['log'])&&(!isset($_POST['mdp']))){
+    echo "ok";
+    header('location:../../pages_erreurs/erreur_403.html');
+}else if((!isset($_POST['log'])||(!isset($_POST['mdp'])))){
+     header('location:../page/err_log.html');
+}
+
+/* Fin script protection */
 
 require __DIR__.'/../../App/App.php';
 require __DIR__.'/../../Cryptage/Cryptage.php';
 
-$query = "Select * from user where log_user = ?";
 
-if(!isset($_POST['log'])){  $log = null;}else $log  = $_POST['log']; // on récupère le login if exist else null
-if(!isset($_POST['mdp'])){  $log = mdp; }else $mdp  = $_POST['mdp']; // on récupère le mdp if exist else null  //On cript le mot de passe 
-$res = App::getDb()->query($query,array($log)); //requete de corresponance ou non
-if($res){  
-    $validPassword = password_verify($mdp, $res[0]['mdp_user']);
-    if($validPassword){
-        $id = $res[0][id_user];
-        session_start();
-        $_SESSION['id_user'] = $id;
-        header('location:../../espace_membre/membre.php');
+
+
+
+if(isset($_POST['log'])&&(isset($_POST['mdp']))){
+    
+    $log  = $_POST['log'];
+    $mdp  = $_POST['mdp'];
+    $query = "Select * from user where log_user = ?";
+    $res = App::getDb()->query($query,array($log)); //requete de corresponance ou non
+    echo count($res);
+    if($res){  
+        $validPassword = password_verify($mdp, $res[0]['mdp_user']);
+        if($validPassword){
+            $id = $res[0][id_user];
+            session_start();
+            $_SESSION['id_user'] = $id;
+            header('location:../../espace_membre/membre.php');
+        }else header('location:../page/err_log.html');
     }else header('location:../page/err_log.html');
-}else echo header('location:../../espace_membre/membre.php');
+}
+
 
     
