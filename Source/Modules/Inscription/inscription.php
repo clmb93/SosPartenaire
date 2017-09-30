2ec39c8a93485f94cf3617b5b5c1a4a05168aa0d
Julien <Julien.ladisa@gmail.com>
2017-09-30 12:16:26 +0200

    dev du control de submit du formulaire inscription dans le inscription.js

--- Source/Modules/Inscription/inscription.php
+++ Source/Modules/Inscription/inscription.php
@@ -10,14 +10,14 @@
 		<div class="col-lg-6 intro-content cache" id="intro-content">
 			<div class=" col-lg-12">
 				<h1 align="center">Inscription</h1><br><br>
-				<form  id="form_inscription" method="post" action="#">
+				<form  id="form_inscription" method="post" action="php/inscrip.php">
 					 <div>
 						<label>Prénom</label>
 						<input type="text" class="form-control" id="prenom" placeholder="Entrez votre nom" name="prenom">
 					  </div><br>
 					  <div class="form-group">
 						<label>Nom</label>
-						<input type="password" class="form-control" id="nom" name="nom" placeholder="Entrez votre prénom">
+						<input type="text" class="form-control" id="nom" name="nom" placeholder="Entrez votre prénom">
 					  </div>
 						<div class="form-group">
 						<label>Date de naissance</label>
@@ -38,7 +38,7 @@
 					  </div>
 					 <div class="form-group">
 						<label for="">Ville</label>
-						<input type="password" class="form-control" id="ville" name="ville" placeholder="Entrez votre ville">
+						<input type="text" class="form-control" id="ville" name="ville" placeholder="Entrez votre ville">
 					  </div>
 					 <div class="form-group">
 						<label for="">Pays</label>
@@ -51,10 +51,10 @@
 					  </div>
 					 <div class="form-group">
 						<label>Mot de passe</label>
-						 <input type="password" class="form-control" name="log" id="log" placeholder="Entrez votre mot de passe">
+						 <input type="password" class="form-control" name="mdp" id="mdp" placeholder="Entrez votre mot de passe">
 					  </div>
                                         <hr>
-                                         <div class="form-check">
+                                         <div class="form-check" id="form_check">
                                             <label class="form-check-label">
                                                 <input class="form-check-input" type="checkbox" value="1" name="conditions" id="conditions">
                                                 J'accepte les <a href="#">conditions utilisateurs</a>
--- Source/Modules/Inscription/js/inscription.js
+++ Source/Modules/Inscription/js/inscription.js
@@ -1,11 +1,84 @@
 $(document).ready(function(){
-	$i=0;
-	
-	$('#intro-content').animate({	marginBottom : "25%"	},1500);
-	$.getJSON( "liste_pays/pays.json", function( data ) {
-	  var items = [];
+    
+	 /* Différentes valeurs du formulaires */
+        
+            $prenom             = $('#prenom');
+            $nom                = $('#nom');
+            $dateNaissance      = $('#datenaissance');
+            $tel                = $('#tel');
+            $email              = $('#email');
+            $adresse            = $('#adresse');
+            $ville              = $('#ville');
+            $pays               = $('#pays');
+            $log                = $('#log');
+            $mdp                = $('#mdp');
+            $conditions         = $('#conditions');
+            
+            
+	$('#intro-content').animate({	marginBottom : "25%" },1500);
+        
+	$.getJSON( "liste_pays/pays.json", function( data ) {   //On récupère la liste des pays et on l'affiche dans le select
 	  $.each( data, function(key,value) {
 		$('#pays').append('<option value='+value+'>'+value+'</option>')
 	  });
 	});
+        
+       
+        
+        $('#form_inscription').on('submit',function(e){
+            
+           $isCocher = $conditions.is(':checked');
+            if((!$prenom.val())||(!$nom.val())||
+                    (!$dateNaissance.val())||(!$tel.val())||
+                        (!$email.val())||(!adresse.val())||(!$ville.val())||
+                            (!$pays.val())||(!$log.val())||(!$mdp.val())||(!$isCocher)){    //longue conditions qui vérifie que tous les champs sont bien remplis et que le bouton checkbox est coché
+                        
+                e.preventDefault();
+                if(!$prenom.val()){
+                   $prenom.addClass('manquant');     
+                } 
+                if(!$nom.val()){
+                     $nom.addClass('manquant');   
+                }
+                if(!$dateNaissance.val()){
+                    $dateNaissance.addClass('manquant');
+                }
+                
+                if(!$tel.val()){
+                     $tel.addClass('manquant');
+                }
+                
+                if(!$email.val()){
+                     $email.addClass('manquant');
+                }
+                
+                if(!$adresse.val()){
+                     $adresse.addClass('manquant');
+                }
+                
+                if(!$ville.val()){
+                     $ville.addClass('manquant');
+                }
+                
+                if(!$pays.val()){
+                     $pays.addClass('manquant');
+                }
+                
+                if(!$log.val()){
+                     $log.addClass('manquant');
+                }
+                
+                 if(!$mdp.val()){
+                     $mdp.addClass('manquant');
+                }
+                
+                if(!$isCocher){
+                     $('#form_check').addClass('manquant');
+                }
+            }
+            
+            
+            
+            
+        });
 });
